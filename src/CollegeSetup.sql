55c991d0a48162a6849e61e2b76b79b50f933db9
Viral-23 <73192398+Viral-23@users.noreply.github.com>
2023-04-08 19:15:50 -0400

    initial commit

---
+++ src/CollegeSetup.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS College;
+USE College;
+
+CREATE TABLE IF NOT EXISTS Departments (
+depName VARCHAR(20), 
+campus VARCHAR(20),
+PRIMARY KEY(depName)
+);
+
+CREATE TABLE IF NOT EXISTS Students (
+first_name VARCHAR(20), 
+last_name VARCHAR(20), 
+id char(9),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Classes (
+className VARCHAR(20), 
+credits char(1), 
+PRIMARY KEY (className)
+);
+
+CREATE TABLE IF NOT EXISTS Majors (
+sid char(9),
+dname VARCHAR(20), 
+FOREIGN KEY (sid) REFERENCES Students (id),
+FOREIGN KEY (dname) REFERENCES Departments (depName)
+);
+
+CREATE TABLE IF NOT EXISTS Minors (
+sid char(9),
+dname VARCHAR(20), 
+FOREIGN KEY (sid) REFERENCES Students (id),
+FOREIGN KEY (dname) REFERENCES Departments (depName)
+);
+
+CREATE TABLE IF NOT EXISTS IsTaking (
+sid char(9),
+cname VARCHAR(20), 
+FOREIGN KEY (sid) REFERENCES Students (id),
+FOREIGN KEY (cname) REFERENCES Classes (className)
+);
+
+CREATE TABLE IF NOT EXISTS HasTaken (
+sid char(9),
+cname VARCHAR(20), 
+grade char(1),
+FOREIGN KEY (sid) REFERENCES Students (id),
+FOREIGN KEY (cname) REFERENCES Classes (className)
+);
